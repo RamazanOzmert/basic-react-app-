252088429a044a5ef87610c66e9639370c15e956
RamazanOzmert <ramazanozmert3@gmail.com>
2023-04-16 01:50:49 +0300

    Updates

---
+++ precache-manifest.45d93149fc15fdd4323a3f51d25ab3f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1491e4ed9aeb318c4c50",
+    "url": "/basic-react-app-/static/css/main.7de2f2eb.chunk.css"
+  },
+  {
+    "revision": "1491e4ed9aeb318c4c50",
+    "url": "/basic-react-app-/static/js/main.d6c4b141.chunk.js"
+  },
+  {
+    "revision": "db0bbebad252ffa122bb",
+    "url": "/basic-react-app-/static/js/runtime~main.ed8e2e64.js"
+  },
+  {
+    "revision": "cb1e567433c199785c98",
+    "url": "/basic-react-app-/static/js/2.5a1b3d3c.chunk.js"
+  },
+  {
+    "revision": "629666658164b43f59b6",
+    "url": "/basic-react-app-/static/js/3.45bd1f93.chunk.js"
+  },
+  {
+    "revision": "2b498fd83e4eb44ab2452c47064bbfab",
+    "url": "/basic-react-app-/static/media/bg.2b498fd8.jpg"
+  },
+  {
+    "revision": "05de04065c1d36a37624d444f3dcb7d0",
+    "url": "/basic-react-app-/index.html"
+  }
+];
